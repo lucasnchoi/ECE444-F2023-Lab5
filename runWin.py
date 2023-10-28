ece13dde794d96514ab6bf73f5a23c38dd434819
lchoi31 <lucaschoi31@gmail.com>
2023-10-28 18:06:36 -0400

    adding login logout views/routes and corresponding unit tests

--- runWin.py
+++ runWin.py
@@ -9,3 +9,5 @@ from project.app import app
 if __name__ == '__main__':
     #using loopback address and default flask port
     app.run(host='127.0.0.1', port=5000) #can set debug by adding debug=True
+
+#note reusing code I made for the project alrdy, just automates opening flask app
